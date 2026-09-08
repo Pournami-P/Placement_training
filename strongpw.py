64a2dd40a597944f8254e34d596800f13cf8e455
Pournami P <pournamip2005@gmail.com>
2026-09-08 09:27:07 +0530

    New updates

--- strongpw.py
+++ strongpw.py
@@ -7,3 +7,33 @@
 # The password should contain at least one lowercase letter, one uppercase letter, and one digit.
 # The password should not contain three repeating characters in a row.
 # The function returns the minimum number of characters that need to be added to the password to make it strong.
+
+s = input()
+
+add = 0
+
+if len(s) < 6:
+    add = 6 - len(s)
+
+lower = False
+upper = False
+digit = False
+
+for ch in s:
+    if ch.islower():
+        lower = True
+    elif ch.isupper():
+        upper = True
+    elif ch.isdigit():
+        digit = True
+
+if not lower:
+    add += 1
+
+if not upper:
+    add += 1
+
+if not digit:
+    add += 1
+
+print(add)
